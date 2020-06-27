034c8905ab493229fb53d02d5fffd515cf5a31c2
Matt Grossman <medg73@gmail.com>
2020-06-27 15:38:09 -0700

    load treasure data from file

---
+++ src/main/java/com/medg/treasuretables/data/SimpleTreasureTable.java
@@ -0,0 +1,13 @@
+package com.medg.treasuretables.data;
+
+public class SimpleTreasureTable {
+    private SimpleTreasureType[] treasureTypes;
+
+    public SimpleTreasureTable() {
+        // no-arg constructor
+    }
+
+    public SimpleTreasureType[] getTreasureTypes() {
+        return treasureTypes;
+    }
+}
